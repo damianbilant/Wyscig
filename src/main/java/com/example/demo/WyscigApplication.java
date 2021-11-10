d40902f560f2a6f010b4ca1e03cec273706c9020
Damian Biłant <damian.bilant@gmail.com>
2021-11-10 17:49:23 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/WyscigApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WyscigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WyscigApplication.class, args);
+	}
+
+}
