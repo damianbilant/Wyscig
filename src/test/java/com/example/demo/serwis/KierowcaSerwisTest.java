b3027650cf410bcdb392614c1b1bf8fe822ae956
Damian Biłant <damian.bilant@gmail.com>
2022-01-30 13:40:17 +0100

    WyscigSerwis aktualizacja

---
+++ src/test/java/com/example/demo/serwis/KierowcaSerwisTest.java
@@ -0,0 +1,113 @@
+package com.example.demo.serwis;
+
+
+import com.example.demo.model.Kierowca;
+import com.example.demo.model.Samochod;
+import com.example.demo.model.TypKierowcy;
+import com.example.demo.model.TypSamochodu;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.internal.matchers.InstanceOf;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@RunWith(JUnitParamsRunner.class)
+public class KierowcaSerwisTest {
+
+
+    @Before
+    public void jestemPrzedTestem() {
+        System.out.println("jestem przed testem");
+    }
+
+    @After
+    public void jestemPoTescie() {
+        System.out.println("jestem po teście");
+    }
+
+    /*    @Mock
+        private KierowcaSerwis kierowcaSerwis;
+        @InjectMocks
+        private KierowcaSerwis kierowcaSerwis;*/
+    @Test
+    @Parameters({
+            "5|jakis tekst", "54|cokolwiek"
+    })
+    public void sprawdzenieParametrow(Integer liczba, String tekst) {
+        //when
+        System.out.println(liczba);
+        System.out.println(tekst);
+        //then
+        Assert.assertNotNull(liczba);
+        Assert.assertNotNull(tekst);
+    }
+
+
+    @Test
+    public void powinnoStworzycKierowce() {
+        //given
+        KierowcaSerwis kierowcaSerwis = new KierowcaSerwis();
+        TypKierowcy typKierowcy = TypKierowcy.UBER;
+        //when
+        Kierowca kierowca = kierowcaSerwis.stworzKierowce(typKierowcy);
+        //then
+        Assert.assertNotNull(kierowca);
+        Assert.assertEquals(TypKierowcy.UBER, kierowca.getTypKierowcy());
+        Assert.assertNotNull(kierowca.getZnajomoscTrasy());
+
+    }
+
+    @Test
+    public void powinnoStworzycKierowce2() {
+        //given
+        KierowcaSerwis kierowcaSerwis = new KierowcaSerwis();
+        TypKierowcy typKierowcy = TypKierowcy.UBER;
+        //when
+        Kierowca kierowca = kierowcaSerwis.stworzKierowce(typKierowcy);
+        //then
+        Assert.assertNotNull(kierowca);
+        Assert.assertEquals(TypKierowcy.UBER, kierowca.getTypKierowcy());
+        Assert.assertNotNull(kierowca.getZnajomoscTrasy());
+
+    }
+
+//zrobić losowe tworzenie kierowcy, losowe tworzenie samochodu i przetestować to/ ewentualnie test dla samochodu bez losowania
+
+    @Test
+    public void powinnoStworzycSamochod() {
+        //given
+        SamochodSerwis samochodSerwis = new SamochodSerwis();
+        TypSamochodu typSamochodu = TypSamochodu.COUPE;
+        //when
+        Samochod samochod = samochodSerwis.stworzSamochod(typSamochodu);
+        //then
+        Assert.assertNotNull(samochod);
+        Assert.assertEquals(TypSamochodu.COUPE, samochod.getTypSamochodu());
+        Assert.assertNotNull(samochod.getCzasPrzejazdu());
+
+    }
+
+    @Test
+    public  void powinnoWylosowacIstworzycKierowce(){
+        //given
+        KierowcaSerwis kierowcaSerwis = new KierowcaSerwis();
+        ArrayList<TypKierowcy> listaTypowKierowcy = new ArrayList<>(Arrays.asList(TypKierowcy.values()));
+        //when
+        Kierowca kierowca = kierowcaSerwis.losowoStworzKierowce();
+        //then
+        Assert.assertNotNull(kierowca);
+        Assert.assertTrue(listaTypowKierowcy.contains(kierowca.getTypKierowcy()));
+        Assert.assertTrue(kierowca instanceof Kierowca);
+    }
+}
